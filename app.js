623f11b7ba1d92f00e6650c3754117ebf0b79af0
Rahul Sharma <pt.rahulsharma67@gmail.com>
2023-01-02 15:55:58 +0530

    update

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post("/", function(req, res){
        if (response.statusCode === 200){
         res.sendFile(__dirname + "/success.html");
        }else{
-        res.sendFile(__dirname + "/failure.html");
+        res.sendFile(__dirname + "/failure.html")
        }
     
         response.on("data", function(data){
