2a5d01d9a61848c6b0b02ca537566321068a7589
Rahul Sharma <92357155+Rahulqqq@users.noreply.github.com>
2023-01-20 10:50:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const { rawListeners } = require("process");
 
 const app = express();
 
+//Node.js body parsing middleware.
 
+//Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
